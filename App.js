153082d429b931965cde5e2ff1f645701e332e0c
Aluno <thayna.matos.carlos@gmail.com>
2025-06-11 13:36:43 -0300

    Switch

--- App.js
+++ App.js
@@ -1,16 +1,47 @@
-import { View, StatusBar, FlatList, Text } from 'react-native';
+import { View, StatusBar, Switch, Text } from 'react-native';
+
+// IMPORT USESTATE
+import { useState } from "react";
 
 // IMPORT STYLES
 import { styles } from './src/styles/styles';
 
 export default function App() {
 
+  const [favorites, setFavorites] = useState(false)
+  const [completed, setCompleted] = useState(false)
+
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>FILTROS</Text>
+
+      <View style={styles.filter}>
+        <Text style={styles.text}>Mostrar Favoritos</Text>
+        <Switch
+          value={favorites}
+          onValueChange={() => setFavorites(!favorites)}
+          thumbColor={favorites ? "green" : "white"}
+          trackColor={{ false: "gray", true: "gray" }}
+        />
+      </View>
+
+      <View style={styles.filter}>
+        <Text style={styles.text}>Mostrar Tarefas Completadas</Text>
+        <Switch
+          value={completed}
+          onValueChange={() => setCompleted(!completed)}
+          thumbColor={completed ? "green" : "white"}
+          trackColor={{ false: "gray", true: "gray" }}
+        />
+
+      </View>
+
+      <Text style={{ marginTop: 10 }}>Filtros Ativos</Text>
+      {favorites && <Text style={{ fontSize: 20, marginTop: 10, fontWeight: "bold" }}>Favoritos!</Text>}
+      {completed && <Text style={{ fontSize: 20, marginTop: 10, fontWeight: "bold" }}>Tarefas Completas!</Text>}
 
       <StatusBar hidden></StatusBar>
     </View>
 
   );
 }
-
--- src/styles/styles.js
+++ src/styles/styles.js
@@ -3,7 +3,9 @@ import { StyleSheet } from "react-native";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#a295f5',
+    backgroundColor: '#bb93c9',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 
   box: {
@@ -14,9 +16,24 @@ export const styles = StyleSheet.create({
   },
 
   text: {
-    color: "white",
+    color: "black",
     fontSize: 15,
+    marginRight: 10,
+
+  },
+
+  title: {
+    color: "black",
+    fontSize: 24,
     fontWeight: "bold",
-  }
+    marginBottom: 20,
+  },
+
+  filter: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
+    marginBottom: 10
+  },
 });
 
