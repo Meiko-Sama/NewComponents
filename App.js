b9cfe3036bdc075efee4713fda08f04bfa3fa2c3
Aluno <thayna.matos.carlos@gmail.com>
2025-06-11 10:48:11 -0300

    Implementando Scroll

--- App.js
+++ App.js
@@ -1,20 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, ScrollView } from 'react-native';
+
+// IMPORT STYLES
+import { styles } from './src/styles/styles';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <ScrollView showsVerticalScrollIndicator={false}>
+
+        <View style={[styles.box, { backgroundColor: "#8578d6" }]}>
+          <Text style={styles.text}> Primeira Caixa!</Text>
+        </View>
+
+        <View style={[styles.box, { backgroundColor: "#7d72c4" }]}>
+          <Text style={styles.text}> Segunda Caixa!</Text>
+        </View>
+
+        <View style={[styles.box, { backgroundColor: "#7065b8" }]}>
+          <Text style={styles.text}> Terceira Caixa!</Text>
+        </View>
+
+        <View style={[styles.box, { backgroundColor: "#6459ab" }]}>
+          <Text style={styles.text}> Quarta Caixa!</Text>
+        </View>
+
+        <View style={[styles.box, { backgroundColor: "#584e9c" }]}>
+          <Text style={styles.text}> Quinta Caixa!</Text>
+        </View>
+
+      </ScrollView>
     </View>
+
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/styles/styles.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#a295f5',
+  },
+
+  box: {
+    height: 250,
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  text: {
+    color: "white",
+    fontSize: 15,
+    fontWeight: "bold",
+  }
+});
+
